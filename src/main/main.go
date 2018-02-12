35abc4059cecca9e81c7823322bfcee05afa035a
Rasent <isusion@163.com>
2018-02-12 19:22:00 +0800

    [ADD]修改main函数可以在idea中运行

--- src/main/main.go
+++ src/main/main.go
@@ -1,57 +1,116 @@
 package main
 
 import (
-	"bufio"
-	"carrier/commands"
-	"carrier/protocol"
-	"fmt"
-	"socket"
+	"carrier/cluster/cluster"
+	_ "carrier/command/set"
+	"carrier/logger"
+	"flag"
+	"github.com/go-ini/ini"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"runtime"
 	"time"
 	"util"
+	"socket"
+	"net"
+	carrierConn "carrier/conn"
 )
 
-func main() {
+var (
+	confpath      = flag.String("conf", "conf/carrier.ini", "配置文件路径")
+	cfg           *ini.File
+	carrierLogger *util.Logger
+)
 
-	c := commands.NewCommandTree()
-	c.AddCommand([]byte("aBcD"), commands.NewCommand())
-
-	fmt.Println(c.SearchCommand([]byte("ABCD")))
-	fmt.Println(c.SearchCommand([]byte("AbCD")))
-	fmt.Println(c.SearchCommand([]byte("ABCd")))
-	fmt.Println(c.SearchCommand([]byte("ABC")))
-
-	for i := 'a'; i <= 'z'; i++ {
-		j := util.ToUpper(byte(i))
-		k := util.ToLower(j)
-		fmt.Println(i, j, k)
-	}
-	return
-	socketClent, err := socket.NewTClientSocket("10.10.10.227:6679")
-	//	socketClent, err := socket.NewTClientSocket("127.0.0.1:6679")
-	fmt.Println(socketClent, err)
-	conn, err := socketClent.DialTimeout(time.Duration(3 * time.Second))
-	fmt.Println(conn, err)
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	flag.Parse()
 
 	var (
-		br     *bufio.Reader    = bufio.NewReader(conn)
-		bw     *bufio.Writer    = bufio.NewWriter(conn)
-		msgAck protocol.Message = protocol.NewMessage()
+		err error
 	)
-
-	// 发数据
-	var rawBytes []byte = []byte("*1\r\n$4\r\nINFO\r\n")
-	//var rawBytes []byte = []byte{42, 50, 13, 10, 36, 55, 13, 10, 99, 108, 117, 115, 116, 101, 114, 13, 10, 36, 53, 13, 10, 115, 108, 111, 116, 115, 13, 10}
-	//var rawBytes []byte = []byte("*2\r\n$7\r\nCLUSTER\r\n$5\r\nSLOTS\r\n")
-	fmt.Println(string(rawBytes))
-	if _, err = bw.Write(rawBytes); err != nil {
-		fmt.Println(err)
-		return
+	if cfg, err = ini.Load(*confpath); err != nil {
+		panic(err)
 	}
-	bw.Flush()
 
-	// 收数据
-	if err = msgAck.ReadOne(br); err != nil {
-		fmt.Println(err)
+	if cfg.Section("PPROF").Key("UsePprof").MustBool(false) {
+		go func() {
+			log.Println(http.ListenAndServe(cfg.Section("PPROF").Key("PprofAddr").MustString(":6063"), nil))
+		}()
 	}
-	fmt.Println(msgAck)
+}
+
+func ServeForClient(listenAddr string) {
+	var (
+		serverSocket *socket.TServerSocket
+		serverConn   net.Conn
+		err          error
+	)
+	if serverSocket, err = socket.NewTServerSocket(listenAddr); err != nil {
+		panic(err)
+	}
+	if err = serverSocket.Listen(); err != nil {
+		panic(err)
+	}
+	defer serverSocket.Close()
+
+	// AcceptLoop
+	for {
+		if serverConn, err = serverSocket.Accept(); err != nil {
+			logger.Notice("Socket To Client Accept: ", err)
+			continue
+		}
+		go func(connClient net.Conn) {
+			defer func() {
+				logger.Info("客户端断开连接: ", connClient.RemoteAddr().String())
+				connClient.Close()
+			}()
+			logger.Info("客户端新连接: ", connClient.RemoteAddr().String())
+			ClientLogic(connClient)
+		}(serverConn)
+	}
+}
+
+
+func ClientLogic(connClient net.Conn) {
+	// TODO 对连接做处理(IP限制什么鬼的)
+	// TODO AUTH
+
+	var (
+		connCarrier carrierConn.Conn
+	)
+	connCarrier = carrierConn.NewConn(connClient)
+	<- connCarrier.IsClosed()
+}
+
+
+func main() {
+	carrierLogger = util.NewLogger(cfg.Section("LOG").Key("Path").MustString(util.HomeDir()+"/logs/carrier.log"), util.FilenameSuffixInDay)
+	logger.SetLogger(carrierLogger)
+
+	var (
+		clusterServerList []string
+		err               error
+		addr              *ini.Key
+	)
+	for _, addr = range cfg.Section("CLUSTER.HOST").Keys() {
+		clusterServerList = append(clusterServerList, addr.Value())
+	}
+
+	// 初始化 cluster
+	if err = cluster.InitClusterParameter(
+		clusterServerList,
+		time.Duration(cfg.Section("CLUSTER").Key("RefreshInterval").MustInt(300))*time.Second,
+		cfg.Section("CLUSTER").Key("MaxIdle").MustInt(50),
+		time.Duration(cfg.Section("CLUSTER").Key("TestOnBorrowTimeout").MustInt(150))*time.Second,
+		time.Duration(cfg.Section("CLUSTER").Key("ConnectTimeout").MustInt(3))*time.Second,
+		time.Duration(cfg.Section("CLUSTER").Key("ReadTimeout").MustInt(3))*time.Second,
+		time.Duration(cfg.Section("CLUSTER").Key("WriteTimeout").MustInt(3))*time.Second,
+	); err != nil {
+		panic(err)
+	}
+
+	// 服务端口 client
+	ServeForClient(cfg.Section("").Key("ClientAddr").MustString(":6679"))
 }
